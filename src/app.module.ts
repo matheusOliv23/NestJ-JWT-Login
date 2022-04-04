61f33d0a5e58b781a4306457dbdea771e5b61d24
matheusoliv23 <matheushenri1996@gmail.com>
2022-04-04 12:09:21 -0300

    Alterando importação do mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
